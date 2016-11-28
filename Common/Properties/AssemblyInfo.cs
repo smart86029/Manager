46d56b6afd12bdf122b3ac2dcf0f8564a0831923
smart86029 <smart86029@gmail.com>
2016-11-28 15:30:50 +0800

    修改組件資訊

--- Common/Properties/AssemblyInfo.cs
+++ Common/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 組件的一般資訊是由下列的屬性集控制。
 // 變更這些屬性的值即可修改組件的相關
 // 資訊。
-[assembly: AssemblyTitle("Common")]
+[assembly: AssemblyTitle("Manager.Common")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Common")]
+[assembly: AssemblyProduct("Manager.Common")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
