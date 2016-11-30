4f2cceb56893be18f1d5cf5f2db0689eaee88f3d
smart86029 <smart86029@gmail.com>
2016-11-30 19:49:08 +0800

    新增使用者相關 Repository

---
+++ Data/EntityFramework/MenuRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+using Manager.Models;
+
+namespace Manager.Data.EntityFramework
+{
+    public class MenuRepository : Repository<Menu>, IMenuRepository
+    {
+        public MenuRepository(DbContext db) : base(db)
+        {
+        }
+    }
+}
