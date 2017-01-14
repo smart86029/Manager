df87f73814af7c9662c0722a04824e1950321698
smart86029 <smart86029@gmail.com>
2017-01-14 10:29:32 +0800

    使用 hash

--- Web/app/app-routing.module.js
+++ Web/app/app-routing.module.js
@@ -24,7 +24,7 @@ var AppRoutingModule = (function () {
     }
     AppRoutingModule = __decorate([
         core_1.NgModule({
-            imports: [router_1.RouterModule.forRoot(routes)],
+            imports: [router_1.RouterModule.forRoot(routes, { useHash: true })],
             exports: [router_1.RouterModule]
         }), 
         __metadata('design:paramtypes', [])
