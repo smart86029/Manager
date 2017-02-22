fd6ac34bf73f791d456da08ec960a24943ba1411
smart86029 <smart86029@gmail.com>
2017-02-22 23:26:36 +0800

    修改 UserService

--- Service/Service/UserService.cs
+++ Service/Service/UserService.cs
@@ -1,31 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Manager.Data;
 using Manager.Models;
 
 namespace Manager.Service
 {
+    /// <summary>
+    /// 使用者服務。
+    /// </summary>
     public class UserService
     {
         private IUnitOfWork unitOfWork;
         private IUserRepository userRepository;
+        private IRoleRepository roleRepository;
 
-        public UserService(IUnitOfWork unitOfWork, IUserRepository userRepository)
+        /// <summary>
+        /// 初始化 <see cref="UserService"/> 類別的新執行個體。
+        /// </summary>
+        /// <param name="unitOfWork">工作單元。</param>
+        /// <param name="userRepository">使用者倉儲。</param>
+        /// <param name="roleRepository">角色倉儲。</param>
+        public UserService(IUnitOfWork unitOfWork, IUserRepository userRepository, IRoleRepository roleRepository)
         {
             this.unitOfWork = unitOfWork;
             this.userRepository = userRepository;
+            this.roleRepository = roleRepository;
         }
 
-        public async Task<User> GetByIdAsync(int id)
+        /// <summary>
+        /// 取得使用者。
+        /// </summary>
+        /// <param name="id">指定的 Id。</param>
+        /// <returns>符合的使用者。</returns>
+        public async Task<User> GetUserByIdAsync(int id)
         {
             var user = await userRepository.FindAsync(id);
+
             return user;
         }
 
-        public async Task<User> GetByNameAsync(string userName)
+        /// <summary>
+        /// 取得使用者。
+        /// </summary>
+        /// <param name="userName">指定的使用者名稱</param>
+        /// <returns>符合的使用者。</returns>
+        public async Task<User> GetUserByNameAsync(string userName)
         {
             var user = await userRepository.FirstOrDefaultAsync(u => u.UserName == userName);
 
@@ -36,7 +57,7 @@ namespace Manager.Service
         /// 取得使用者，包含角色。
         /// </summary>
         /// <param name="id">指定的 Id。</param>
-        /// <returns></returns>
+        /// <returns>符合的使用者。</returns>
         public async Task<User> GetUserIncludeRolesAsync(int id)
         {
             var user = await userRepository.FirstOrDefaultAsync(u => u.UserId == id, u => u.Roles);
@@ -59,9 +80,13 @@ namespace Manager.Service
         /// 新增使用者。
         /// </summary>
         /// <param name="user">要新增的使用者。</param>
-        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"><paramref name="user"/> 為 null。</exception>
+        /// <returns>新增成功傳回是，否則為否。</returns>
         public async Task<bool> CreateAsync(User user)
         {
+            if (user == null)
+                throw new ArgumentNullException(nameof(user));
+
             userRepository.Create(user);
             await unitOfWork.CommitAsync();
 
@@ -73,9 +98,13 @@ namespace Manager.Service
         /// </summary>
         /// <param name="user">要更新的使用者。</param>
         /// <param name="selectedRoles">角色清單選擇的角色。</param>
-        /// <returns></returns>
+        /// <returns>更新成功傳回是，否則為否。</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="user"/> 為 null。</exception>
         public async Task<bool> UpdateAsync(User user, string[] selectedRoles)
         {
+            if (user == null)
+                throw new ArgumentNullException(nameof(user));
+
             //await UpdateRoles(user, selectedRoles);
             userRepository.Update(user);
             await unitOfWork.CommitAsync();
@@ -87,7 +116,7 @@ namespace Manager.Service
         /// 刪除使用者。
         /// </summary>
         /// <param name="id">指定的 Id。</param>
-        /// <returns></returns>
+        /// <returns>刪除成功傳回是，否則為否。 如果找不到符合的使用者，則這個方法也會傳回否。</returns>
         public async Task<bool> DeleteAsync(int id)
         {
             var user = await userRepository.FindAsync(id);
@@ -100,38 +129,39 @@ namespace Manager.Service
             return true;
         }
 
-        ///// <summary>
-        ///// 更新關聯角色。
-        ///// </summary>
-        ///// <param name="user">使用者。</param>
-        ///// <param name="selectedRoles">角色清單選擇的角色。</param>
-        ///// <exception cref="ArgumentNullException"></exception>
-        //private async Task UpdateRoles(User user, string[] selectedRoles)
-        //{
-        //    if (user == null)
-        //        throw new ArgumentNullException(nameof(user));
-        //    if (selectedRoles == null)
-        //    {
-        //        user.Roles = new List<Role>();
-        //        return;
-        //    }
+        /// <summary>
+        /// 更新關聯角色。
+        /// </summary>
+        /// <param name="user">使用者。</param>
+        /// <param name="selectedRoles">角色清單選擇的角色。</param>
+        /// <exception cref="ArgumentNullException"><paramref name="user"/> 為 null。</exception>
+        private async Task UpdateRoles(User user, string[] selectedRoles)
+        {
+            if (user == null)
+                throw new ArgumentNullException(nameof(user));
 
-        //    var roles = await unitOfWork.RoleRepository.ManyAsync(null);
-        //    var currentRoleIds = user.Roles.Select(r => r.RoleId);
+            if (selectedRoles == null)
+            {
+                user.Roles = new List<Role>();
+                return;
+            }
 
-        //    foreach (var role in roles)
-        //    {
-        //        if (selectedRoles.Contains(role.RoleId.ToString()))
-        //        {
-        //            if (!currentRoleIds.Contains(role.RoleId))
-        //                user.Roles.Add(role);
-        //        }
-        //        else
-        //        {
-        //            if (currentRoleIds.Contains(role.RoleId))
-        //                user.Roles.Remove(role);
-        //        }
-        //    }
-        //}
+            var roles = await roleRepository.ManyAsync(null);
+            var currentRoleIds = user.Roles.Select(r => r.RoleId);
+
+            foreach (var role in roles)
+            {
+                if (selectedRoles.Contains(role.RoleId.ToString()))
+                {
+                    if (!currentRoleIds.Contains(role.RoleId))
+                        user.Roles.Add(role);
+                }
+                else
+                {
+                    if (currentRoleIds.Contains(role.RoleId))
+                        user.Roles.Remove(role);
+                }
+            }
+        }
     }
 }
