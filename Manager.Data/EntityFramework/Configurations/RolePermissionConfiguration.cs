0e3b00cce7dc99acbd10eec23b5d15d778cfbd1c
smart86029 <smart86029@gmail.com>
2018-06-09 01:28:16 +0800

    新增 Permission

---
+++ Manager.Data/EntityFramework/Configurations/RolePermissionConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Manager.Models.System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Manager.Data.EntityFramework.Configurations
+{
+    public class RolePermissionConfiguration : IEntityTypeConfiguration<RolePermission>
+    {
+        public void Configure(EntityTypeBuilder<RolePermission> builder)
+        {
+            builder.ToTable("RolePermission", "System");
+            builder.HasKey(x => new { x.RoleId, x.PermissionId });
+            builder.HasData(GetSeedData());
+        }
+
+        private RolePermission[] GetSeedData()
+        {
+            var result = new RolePermission[]
+            {
+            };
+
+            return result;
+        }
+    }
+}
