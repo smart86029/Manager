9b20624cc9a2f5452d443d8d07e8e4f400b440ae
smart86029 <smart86029@gmail.com>
2018-11-08 11:04:39 +0800

    新增前端專案

---
+++ MatchaLatte.Ordering.Domain/Buyers/Buyer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using MatchaLatte.Common.Domain;
+
+namespace MatchaLatte.Ordering.Domain.Buyers
+{
+    /// <summary>
+    /// 購買者。
+    /// </summary>
+    public class Buyer : Entity, IAggregateRoot
+    {
+        /// <summary>
+        /// 取得主鍵。
+        /// </summary>
+        /// <value>主鍵。</value>
+        public Guid BuyerId { get; private set; }
+
+        /// <summary>
+        /// 取得名。
+        /// </summary>
+        /// <value>名。</value>
+        public string FirstName { get; private set; }
+
+        /// <summary>
+        /// 取得姓。
+        /// </summary>
+        /// <value>姓。</value>
+        public string LastName { get; private set; }
+
+        /// <summary>
+        /// 取得顯示名稱。
+        /// </summary>
+        /// <value>顯示名稱。</value>
+        public string DisplayName => FirstName + LastName;
+    }
+}
