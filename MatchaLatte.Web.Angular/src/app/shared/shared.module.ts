0720b4ab02e848aacd6acd461cb1ed2c5fe599f4
smart86029 <smart86029@gmail.com>
2019-07-19 09:42:22 +0800

    移除 @angular/flex-layout

--- MatchaLatte.Web.Angular/src/app/shared/shared.module.ts
+++ MatchaLatte.Web.Angular/src/app/shared/shared.module.ts
@@ -1,12 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { MaterialModule } from './material.module';
 import { InlineEditorComponent } from './inline-editor/inline-editor.component';
-import { ActionNamePipe } from './save-mode/action-name.pipe';
+import { MaterialModule } from './material.module';
 import { YesNoPipe } from './pipes/yes-no.pipe';
+import { ActionNamePipe } from './save-mode/action-name.pipe';
 
 @NgModule({
   imports: [
@@ -14,13 +13,13 @@ import { YesNoPipe } from './pipes/yes-no.pipe';
     FormsModule,
     ReactiveFormsModule,
     MaterialModule,
-    FlexLayoutModule
+
   ],
   exports: [
     CommonModule,
     FormsModule,
     MaterialModule,
-    FlexLayoutModule,
+
     ActionNamePipe,
     InlineEditorComponent,
     YesNoPipe,
@@ -28,7 +27,7 @@ import { YesNoPipe } from './pipes/yes-no.pipe';
   declarations: [
     ActionNamePipe,
     InlineEditorComponent,
-    YesNoPipe
+    YesNoPipe,
   ]
 })
 export class SharedModule { }
